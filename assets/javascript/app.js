e5d0383adb1b36a7f827a6ec8969654594a64d25
thechampl <luke.champ@outlook.com>
2018-01-20 13:59:19 -0500

    final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,7 +11,7 @@ var questionList = [{
     correctAnswer: "Golf",
 },
 {
-    question:  "Who has won the most Gold medals at a single Olympic Games in the history of the Olympics?",
+    question: "Who has won the most Gold medals at a single Olympic Games in the history of the Olympics?",
     answers: ["James Connelly", "Mark Spitz", "Michael Phelps"],
     correctAnswer: "Michael Phelps",
 },
@@ -22,32 +22,37 @@ var questionList = [{
 }
 ];
 
-var x ="";
+var x = "";
 x = Math.floor(Math.random() * this.questionList.length);
 
-function displayQuestion(){
-$("#question").text(this.questionList[x].question);
-var idNumber = 1;
-		for(i = 0; i < 3; i++){
-			selectionNumber = Math.floor(Math.random() * this.questionList[x].answers.length);
-            var answerId = "#answer" + idNumber;
-            console.log(answerId);
-			idNumber = idNumber + 1;
-			$(answerId).text(this.questionList[x].answers[selectionNumber]);
-			$(answerId).val(this.questionList[x].answers[selectionNumber]);
-            this.questionList[x].answers.splice([selectionNumber],1);}
-            correctAnswer = this.questionList[x].correctAnswer;
-            console.log(correctAnswer);
-        }
-            
-            
-var game = {
-win:  0,
-loss: 0,
-state : ""
+function displayQuestion() {
+    $("#question").empty();
+    $("#question").text(this.questionList[x].question);
+    var idNumber = 1;
+    for (i = 0; i < 3; i++) {
+        selectionNumber = Math.floor(Math.random() * this.questionList[x].answers.length);
+        var answerId = "#answer" + idNumber;
+        console.log(answerId);
+        idNumber = idNumber + 1;
+        $(answerId).text(this.questionList[x].answers[selectionNumber]);
+        $(answerId).val(this.questionList[x].answers[selectionNumber]);
+        this.questionList[x].answers.splice([selectionNumber], 1);
+    }
+    correctAnswer = this.questionList[x].correctAnswer;
+    console.log(correctAnswer);
+    number = 20;
+    run();
+
 }
 
-            
+
+var game = {
+    win: 0,
+    loss: 0,
+    state: ""
+}
+
+
 
 
 
@@ -56,7 +61,7 @@ state : ""
 var number = 20;
 var intervalId;
 function run() {
-    
+
 
     intervalId = setInterval(decrement, 1000);
 }
@@ -65,40 +70,60 @@ function decrement() {
     number--;
     $("#remaining").html("  " + number);
     if (number === 0) {
-        stop();        
+        stop();
     }
-    
+
 }
 
 
-    
+
 function stop() {
     clearInterval(intervalId);
-    game.loss ++;
+    game.loss++;
     $("#wrong").html("WRONG " + game.loss);
-    // $("#remaining").clear();
+
     number = 20;
     $("#remaining").html("" + number);
-    x++;
+    x--;
     displayQuestion();
-    run();
+
 }
 
 displayQuestion();
 
-run();
-
-$(".btn-default").click(function() {
-    alert("test");
-    console.log(this);
-    console.log(this.value);
-
-//  if(this. = correctAnswer)
-
-});  
 
 
+$(".btn-default").click(function () {
+
+    if (this.value === correctAnswer) {
+        game.win++
+        $("#correct").html("CORRECT " + game.win);
+        number = 20;
 
 
-    
+    }
+    else {
+        game.loss++
+        $("#wrong").html("WRONG " + game.loss);
+
+    }
+
+    clearInterval(intervalId);
+    number = 20;
+    $("#remaining").html("" + number);
+    x--;
+
+    displayQuestion()
+
+    console.log(number);
+    console.log(question);
+    console.log(questionList);
+    console.log(correctAnswer);
+});
+
+
+
+
+
+
 
